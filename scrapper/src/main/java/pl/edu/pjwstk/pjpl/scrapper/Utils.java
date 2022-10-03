cf73819e398579648b2837e75f99738c03d9b9c2
Mateusz Budzisz <electroluxv2@gmail.com>
2022-10-03 14:07:36 +0200

    Basic navigation

---
+++ scrapper/src/main/java/pl/edu/pjwstk/pjpl/scrapper/Utils.java
@@ -0,0 +1,55 @@
+package pl.edu.pjwstk.pjpl.scrapper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class Utils {
+    public static List<String> listComboBoxItems(final WebDriver driver, final WebDriverWait wait, final String partialId) {
+        final var comboBoxBy = By.id("%s_Input".formatted(partialId));
+        final var comboBoxDropDownBy = By.id("%s_DropDown".formatted(partialId));
+        openComboBox(driver, wait, comboBoxBy, comboBoxDropDownBy);
+
+        var dropDownElement = driver.findElement(comboBoxDropDownBy);
+        var availableItems = dropDownElement.findElements(By.tagName("li"));
+
+        System.out.printf("Listing all available options for `%s`:%n", partialId);
+        availableItems.forEach(item -> System.out.printf("- `%s`%n", item.getText()));
+
+        return availableItems.stream().map(i -> i.getText().trim()).toList();
+    }
+
+    /**
+     * @Notice: This method does not wait for click result
+     */
+    public static void selectComboBoxItem(final WebDriver driver, final WebDriverWait wait, final String partialId, final String itemText) {
+        final var comboBoxBy = By.id("%s_Input".formatted(partialId));
+        final var comboBoxDropDownBy = By.id("%s_DropDown".formatted(partialId));
+
+        System.out.printf("Selecting `%s` ComboBox item.%n", itemText);
+        openComboBox(driver, wait, comboBoxBy, comboBoxDropDownBy);
+
+        final var comboBoxDropDownElement = driver.findElement(comboBoxDropDownBy);
+        final var items = comboBoxDropDownElement.findElements(By.tagName("li"));
+        // noinspection ResultOfMethodCallIgnored
+        items.stream().map(WebElement::getText).toList(); // Without this, below line won't work for non ascii characters
+        final var foundItem = items.stream().filter(item -> item.getText().trim().equalsIgnoreCase(itemText)).findFirst().orElseThrow();
+
+        foundItem.click();
+
+        wait.until(ExpectedConditions.invisibilityOf(comboBoxDropDownElement)); // Close animation
+    }
+
+    public static void openComboBox(final WebDriver driver, final WebDriverWait wait, final By comboBoxBy, final By comboBoxDropDownBy) {
+        final var comboBoxItemsBy = By.tagName("li");
+        final var comboBoxElement = driver.findElement(comboBoxBy);
+
+        wait.until(ExpectedConditions.elementToBeClickable(comboBoxElement));
+        comboBoxElement.click(); // There is loading animation
+        wait.until(ExpectedConditions.visibilityOfNestedElementsLocatedBy(comboBoxDropDownBy, comboBoxItemsBy));
+    }
+}
