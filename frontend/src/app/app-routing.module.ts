9dd64d0bbc44c03612c14fde5aff79e6223ce630
Mateusz Budzisz <electroluxv2@gmail.com>
2022-10-16 10:05:28 +0200

    Some changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalendarComponent} from "./calendar/calendar.component";
+import {TodayComponent} from "./today/today.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'calendar',
+  component: CalendarComponent
+}, {
+  path: 'today',
+  component: TodayComponent
+}, {
+  path: '**',
+  pathMatch: 'full',
+  redirectTo: 'calendar'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,16 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import { CalendarComponent } from './calendar/calendar.component';
+import { TodayComponent } from './today/today.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    TodayComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +28,9 @@ import {MatToolbarModule} from "@angular/material/toolbar";
       registrationStrategy: 'registerWhenStable:30000'
     }),
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
