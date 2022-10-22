c1df8930e2d28bb1e9fbc859e66c68cde166ed5c
Mateusz Budzisz <electroluxv2@gmail.com>
2022-10-22 19:13:01 +0200

    Not so good filtering

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -1,21 +1,97 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {firstValueFrom, Observable} from "rxjs";
+
+interface Event {
+  from: Date;
+  to: Date;
+  room: string;
+  location: string;
+  groups: string[];
+  color: string;
+  additionalData: Record<string, string>;
+}
+
+interface Metadata {
+  updated: Date;
+  study: string;
+  name: string;
+  semester: string;
+}
+
+export type Subject = Event & Metadata;
+
+export interface IndexItem {
+  name: string;
+  id: string;
+}
+
+export type Semester = IndexItem;
+export type Study = IndexItem;
+export type Group = IndexItem;
+
+export interface Grouped<T> {
+  name: string;
+  items: T[];
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   private static readonly API = "//s2.budziszm.pl/data";
+  public readonly availableSemesters: Semester[] = [];
+  public readonly selectedSemestersIds = new Set<string>();
+  public readonly availableStudies: Grouped<Study>[] = [];
+  public readonly selectedStudiesIds = new Set<string>();
+  public readonly availableGroups: Grouped<Group>[] = [];
+  public readonly selectedGroupsIds = new Set<string>();
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {
+    void this.loadData();
+  }
+
+  private async loadData(): Promise<void> {
+    const semesters: Semester[] = Object
+      .entries(await firstValueFrom(this.loadSemesters()))
+      .map(([name, id]) => ({name, id}));
+
+    this.availableSemesters.push(...semesters);
+
+    for (const semester of semesters) {
+      const studies: Study[] = Object
+        .entries(await firstValueFrom(this.loadStudies(semester.id)))
+        .map(([name, id]) => ({name, id: `${semester.id}/${id}`}));
+
+      this.availableStudies.push({
+        name: semester.name,
+        items: studies
+      });
+
+      for (const study of studies) {
+        const groups: Group[] = Object
+          .entries(await firstValueFrom(this.loadGroups(study.id)))
+          .map(([name, id]) => ({name, id: `${study.id}/${id}`}));
+
+        this.availableGroups.push({
+          name: `${semester.name} - ${study.name}`,
+          items: groups
+        });
+      }
+    }
+  }
 
   public loadSemesters(): Observable<Record<string, string>> {
     return this.getIndex('semesters');
   }
 
-  public loadStudies(semesterId: string): Observable<Record<string, string>> {
-    return this.getIndex(`${semesterId}/studies`);
+  public loadStudies(semester: string): Observable<Record<string, string>> {
+    return this.getIndex(`${semester}/studies`);
+  }
+
+  public loadGroups(semesterWithStudy: string): Observable<Record<string, string>> {
+    return this.getIndex(`${semesterWithStudy}/groups`);
   }
 
   private getIndex(path: string): Observable<Record<string, string>> {
--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
 import {MatOptionSelectionChange} from "@angular/material/core";
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'app-settings',
@@ -9,12 +10,37 @@ import {MatOptionSelectionChange} from "@angular/material/core";
 })
 export class SettingsComponent implements OnDestroy {
   private alive = true;
+  public readonly selectedSemestersIds = Array.from(this.dataService.selectedSemestersIds);
+  public readonly selectedStudiesIds = Array.from(this.dataService.selectedStudiesIds);
+  public readonly selectedGroupsIds = Array.from(this.dataService.selectedGroupsIds);
+
+  constructor(public readonly dataService: DataService) { }
 
   public ngOnDestroy(): void {
     this.alive = false;
   }
 
   public onSemesterSelectionChange({ source: { selected, value: semesterId } }: MatOptionSelectionChange<string>): void {
+    if (selected) {
+      this.dataService.selectedSemestersIds.add(semesterId);
+    } else {
+      this.dataService.selectedSemestersIds.delete(semesterId);
+    }
+  }
 
+  public onStudySelectionChange({ source: { selected, value: studyId } }: MatOptionSelectionChange<string>): void {
+    if (selected) {
+      this.dataService.selectedStudiesIds.add(studyId);
+    } else {
+      this.dataService.selectedStudiesIds.delete(studyId);
+    }
+  }
+
+  public onGroupSelectionChange({ source: { selected, value: groupId } }: MatOptionSelectionChange<string>): void {
+    if (selected) {
+      this.dataService.selectedGroupsIds.add(groupId);
+    } else {
+      this.dataService.selectedGroupsIds.delete(groupId);
+    }
   }
 }
