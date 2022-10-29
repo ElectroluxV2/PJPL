52e1e6aab7d60078f19e09b854308fb116cb4d03
Mateusz Budzisz <electroluxv2@gmail.com>
2022-10-29 12:20:12 +0200

    Group chooser

--- frontend/src/app/services/group.service.ts
+++ frontend/src/app/services/group.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from "rxjs";
-import {Group} from "./data.service";
+import {BehaviorSubject, firstValueFrom, Subject} from "rxjs";
+import {DataService, Group, Semester, Study} from "./data.service";
+
+export type GroupWithMetadata = Group & {
+  semesterName: string; // semester id is part of group id
+  studyName: string; // study id is part of group id
+};
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +13,70 @@ import {Group} from "./data.service";
 export class GroupService {
   private readonly selectedGroups = new Map<string, string>();
   public readonly selectedGroups$ = new BehaviorSubject<Group[]>([]);
+  public readonly availableSemesters$ = new BehaviorSubject<Semester[]>([]);
+  public readonly availableStudies$ = new BehaviorSubject<Study[]>([]);
+  private readonly availableGroups = new Map<string, GroupWithMetadata[]>();
 
-  constructor() { }
+  constructor(private readonly dataService: DataService) {
+    void this.loadAvailableGroups();
+  }
 
-  selectGroup(g1: string) {
-    this.selectedGroups.set(`g-${Math.random()}`, `${Math.random()}`);
+  private async loadAvailableGroups(): Promise<void> {
+    const semesters: Semester[] = Object
+      .entries(await firstValueFrom(this.dataService.loadSemesters()))
+      .map(([name, id]) => ({name, id}));
+
+    this.availableSemesters$.next(semesters);
+
+    const allStudies: Study[] = [];
+
+    for (const semester of semesters) {
+      const studies: Study[] = Object
+          .entries(await firstValueFrom(this.dataService.loadStudies(semester.id)))
+          .map(([name, id]) => ({name, id: `${semester.id}/${id}`}));
+
+      allStudies.push(...studies);
+
+      for (const study of studies) {
+        const groups: GroupWithMetadata[] = Object
+              .entries(await firstValueFrom(this.dataService.loadGroups(study.id)))
+              .map(([name, id]) => ({
+                name,
+                id: `${study.id}/${id}`,
+                semesterName: semester.name,
+                studyName: study.name
+              }));
+
+        this.availableGroups.set(study.id, groups);
+      }
+    }
+
+    this.availableStudies$.next(allStudies);
+  }
+
+  public selectGroup(id: string, name: string): void {
+    this.selectedGroups.set(id, name);
     this.selectedGroups$.next(Array.from(this.selectedGroups.entries()).map(([id, name]) => ({id, name})));
   }
 
-  removeGroup(id: string) {
+  public removeGroup(id: string): void {
     this.selectedGroups.delete(id);
     this.selectedGroups$.next(Array.from(this.selectedGroups.entries()).map(([id, name]) => ({id, name})));
+  }
+
+  public isSelected(groupId: string): boolean {
+    return this.selectedGroups.has(groupId);
+  }
+
+  public getGroups(studiesIds: string[]): GroupWithMetadata[] {
+    return Array.of(studiesIds.map(id => this.availableGroups.get(id)!)).flat(2).sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  public toggle(group: Group): void {
+    if (this.isSelected(group.id)) {
+      this.removeGroup(group.id);
+    } else {
+      this.selectGroup(group.id, group.name);
+    }
   }
 }
--- frontend/src/app/settings/group-chooser/group-chooser.component.ts
+++ frontend/src/app/settings/group-chooser/group-chooser.component.ts
@@ -1,5 +1,9 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {GroupService} from "../../services/group.service";
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {GroupService, GroupWithMetadata} from "../../services/group.service";
+import {MatOptionSelectionChange} from "@angular/material/core";
+import {BehaviorSubject, ReplaySubject} from "rxjs";
+import {Study} from "../../services/data.service";
+import {MatBottomSheetRef} from "@angular/material/bottom-sheet";
 
 @Component({
   selector: 'app-group-chooser',
@@ -7,14 +11,46 @@ import {GroupService} from "../../services/group.service";
   styleUrls: ['./group-chooser.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GroupChooserComponent implements OnInit {
+export class GroupChooserComponent {
+  public readonly selectedSemestersIds$ = new BehaviorSubject<string[]>([]);
+  public readonly filteredStudies$ = new ReplaySubject<Study[]>(1);
+  public readonly selectedStudiesIds$ = new BehaviorSubject<string[]>([]);
+  public readonly filteredGroups$ = new ReplaySubject<GroupWithMetadata[]>(1);
 
-  constructor(private readonly groupService: GroupService) { }
 
-  ngOnInit(): void {
+  constructor(
+    public readonly groupService: GroupService,
+    private sheet: MatBottomSheetRef<GroupChooserComponent>
+  ) { }
+
+  public close(): void {
+    this.sheet.dismiss();
   }
 
-  add() {
-    this.groupService.selectGroup("g1");
+  public onSemesterSelectionChange({ source: { selected, value } }: MatOptionSelectionChange<string>): void {
+    const ids = this.selectedSemestersIds$.value;
+
+    if (!selected) {
+      this.selectedSemestersIds$.next(ids.filter(id => id !== value));
+    } else {
+      this.selectedSemestersIds$.next(ids.concat(value));
+    }
+
+    const filtered = this.groupService.availableStudies$.value.filter(s => this.selectedSemestersIds$.value.some(x => s.id.includes(x)));
+    this.filteredStudies$.next(filtered);
+    this.filteredGroups$.next([]);
+  }
+
+  public onStudySelectionChange({ source: { selected, value } }: MatOptionSelectionChange<string>): void {
+    const ids = this.selectedStudiesIds$.value;
+
+    if (!selected) {
+      this.selectedStudiesIds$.next(ids.filter(id => id !== value));
+    } else {
+      this.selectedStudiesIds$.next(ids.concat(value));
+    }
+
+    const filtered: GroupWithMetadata[] = this.groupService.getGroups(this.selectedStudiesIds$.value);
+    this.filteredGroups$.next(filtered);
   }
 }
