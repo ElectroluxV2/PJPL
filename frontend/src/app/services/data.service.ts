79271df33c371f44c2c7f2c30662903790c79a66
Mateusz Budzisz <electroluxv2@gmail.com>
2022-11-05 23:47:44 +0100

    Save groups data offline

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -1,8 +1,16 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject, firstValueFrom, map, Observable} from "rxjs";
+import {BehaviorSubject, first, firstValueFrom, map, Observable, skip} from "rxjs";
 import * as LZ from 'lz-string';
-import {Subject} from "./api.service";
+import {ApiService, IndexItem, Subject} from "./api.service";
+import {GroupService} from "./group.service";
+
+export interface GroupSyncInfo {
+  groupId: string;
+  groupName: string;
+  updated: number;
+  synchronized: number;
+}
 
 
 @Injectable({
@@ -12,95 +20,60 @@ export class DataService {
   private readonly subjects = new Map<number, Subject[]>();
   public readonly subjects$ = new BehaviorSubject<Map<number, Subject[]>>(new Map());
 
+  private groupSyncInfo: GroupSyncInfo[] = [];
+  public readonly groupSyncInfo$ = new BehaviorSubject<GroupSyncInfo[]>([]);
+
   public firstValuableDay: number = Number.MAX_VALUE;
   public lastValuableDay: number = 0;
 
-  constructor() {
-    // this.sync().then(() => );
-    this.load();
-
-    // console.log(new Date(new Date(1664969400 * 1000).toDateString()).valueOf())
+  constructor(
+    private readonly groupsService: GroupService,
+    private readonly apiService: ApiService
+  ) {
+    this.groupsService.selectedGroups$
+      // if group will not be found in storage it will be downloaded, otherwise it will just load faster but will refresh anyway, we do this in parallel
+      .subscribe(savedGroups => savedGroups.forEach(group => void this.loadGroupFromStorage(group.id)));
   }
 
-  private load(): void {
-    console.time('load');
+  private async synchronizeGroup(groupId: string): Promise<void> {
+    const subjects: Subject[] = await firstValueFrom(this.apiService.loadSubjects(groupId));
+    this.putSubjectsInMap(subjects);
+    console.log(`Loaded ${subjects.length} subjects for group ${groupId} from api.`);
+    const json = JSON.stringify(subjects);
+    const compressed = LZ.compress(json);
+    localStorage.setItem(`G-${groupId}`, compressed);
+  }
 
-    const cachedGroups = Object.keys(localStorage).filter(key => key.includes('G-'));
+  private async loadGroupFromStorage(groupId: string): Promise<void> {
+    if (!(`G-${groupId}` in localStorage)) {
+      console.warn(`Group ${groupId} not found in storage, synchronizing now.`);
+      return this.synchronizeGroup(groupId);
+    }
 
-    for (const groupKey of cachedGroups) {
-      const compressed = localStorage.getItem(groupKey)!;
-      const json = LZ.decompress(compressed)!;
-      const subjects: Subject[] = JSON.parse(json);
+    const compressed = localStorage.getItem(`G-${groupId}`)!;
+    const json = LZ.decompress(compressed)!;
+    const subjects: Subject[] = JSON.parse(json) ?? [];
+    console.log(`Loaded ${subjects.length} subjects for group ${groupId} from localStorage.`);
+    this.putSubjectsInMap(subjects);
 
-      for (const subject of subjects) {
-        const from = subject.from * 1000;
-        const timestamp = new Date(new Date(from).toDateString()).valueOf(); // Date without time
+    // Load fresh data anyway, used by automated synchronizer
+    return this.synchronizeGroup(groupId);
+  }
 
-        if (!this.subjects.has(timestamp)) {
-          this.subjects.set(timestamp, []);
-        }
+  private putSubjectsInMap(subjects: Subject[]): void {
+    for (const subject of subjects) {
+      const timestamp = new Date(new Date(subject.from * 1000).toDateString()).valueOf(); // Date without time
 
-        this.subjects.get(timestamp)!.push(subject);
-
-        this.firstValuableDay = Math.min(this.firstValuableDay, timestamp);
-        this.lastValuableDay = Math.max(this.lastValuableDay, timestamp);
+      if (!this.subjects.has(timestamp)) {
+        this.subjects.set(timestamp, []);
       }
+
+      this.subjects.get(timestamp)!.push(subject);
+
+      this.firstValuableDay = Math.min(this.firstValuableDay, timestamp);
+      this.lastValuableDay = Math.max(this.lastValuableDay, timestamp);
     }
 
     this.subjects$.next(this.subjects);
-
-    console.timeEnd('load');
-  }
-
-  private async sync(): Promise<void> {
-    // console.log('Sync start')
-    // console.time('sync');
-    //
-    // const semesters: Semester[] = Object
-    //   .entries(await firstValueFrom(this.loadSemesters()))
-    //   .map(([name, id]) => ({name, id}));
-    //
-    // this.availableSemesters.push(...semesters);
-    //
-    // for (const semester of semesters) {
-    //   const studies: Study[] = Object
-    //     .entries(await firstValueFrom(this.loadStudies(semester.id)))
-    //     .map(([name, id]) => ({name, id: `${semester.id}/${id}`}));
-    //
-    //   this.availableStudies.push({
-    //     name: semester.name,
-    //     items: studies
-    //   });
-    //
-    //   for (const study of studies) {
-    //     const groups: Group[] = Object
-    //       .entries(await firstValueFrom(this.loadGroups(study.id)))
-    //       .map(([name, id]) => ({name, id: `${study.id}/${id}`}));
-    //
-    //     this.availableGroups.push({
-    //       name: `${semester.name} - ${study.name}`,
-    //       items: groups
-    //     });
-    //
-    //     for (const group of groups) {
-    //       const subjects: Subject[] = await firstValueFrom(this.loadSubjects(group.id));
-    //       const json = JSON.stringify(subjects);
-    //       const compressed = LZ.compress(json);
-    //       localStorage.setItem(`G-${group.id}`, compressed);
-    //
-    //       // for (const subject of subjects) {
-    //       //   const key = new Date(new Date(subject.from * 1000).toDateString()).valueOf(); // Date without time
-    //       //
-    //       //   if (!this.subjects.has(key)) {
-    //       //     this.subjects.set(key, []);
-    //       //   }
-    //       //
-    //       //   this.subjects.get(key)!.push(subject);
-    //       // }
-    //     }
-    //   }
-    // }
-    //
-    // console.timeEnd('sync');
   }
 }
