2e5d91b602bd4ca3957f7b3c9d78e8b48622c857
ElectroluxV2 <electroluxv2@gmail.com>
2022-11-06 22:05:32 +0100

    Use remote webdriver

--- scrapper/src/main/java/pl/edu/pjwstk/pjpl/scrapper/SeleniumFactory.java
+++ scrapper/src/main/java/pl/edu/pjwstk/pjpl/scrapper/SeleniumFactory.java
@@ -1,29 +1,18 @@
 package pl.edu.pjwstk.pjpl.scrapper;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.service.DriverService;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.logging.Level;
 
 public class SeleniumFactory {
-    public static WebDriver makeDriver() {
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+    public static WebDriver makeDriver() throws MalformedURLException {
         java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.WARNING);
-
-        DriverService.Builder<ChromeDriverService, ChromeDriverService.Builder> serviceBuilder = new ChromeDriverService.Builder().withSilent(true);
-        ChromeDriverService chromeDriverService = serviceBuilder.build();
-        try {
-            chromeDriverService.sendOutputTo(new FileOutputStream("webdriver.logs"));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
         final var options = new ChromeOptions();
         options.addArguments("--headless");
@@ -31,7 +20,7 @@ public class SeleniumFactory {
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--log-level=3");
 
-        return new ChromeDriver(chromeDriverService, options);
+        return new RemoteWebDriver(new URL(System.getenv("PJPL_webdriver")), options);
     }
 
     public static WebDriverWait makeWait(final WebDriver driver) {
