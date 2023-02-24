3a4a8a4d8e7dc05a595d6d124be6cfc76d037d53
ElectroluxV2 <electroluxv2@gmail.com>
2023-02-24 21:20:16 +0100

    twa

---
+++ android/app/src/main/java/pjpl/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package pjpl.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
